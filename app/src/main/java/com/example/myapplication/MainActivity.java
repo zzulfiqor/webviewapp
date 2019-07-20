f36b1e1baa8a889a58a7e246337359907ed9ddae
zzulfiqor <zzulfiqor@gmail.com>
2019-07-20 22:32:46 +0700

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+//        view.loadUrl("https://zzulfiqor.github.io/");
+        view.loadUrl("http://hadits.santrisoft.com/");
+
+
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //ketika disentuh tombol back
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack(); //method goback(),untuk kembali ke halaman sebelumnya
+            return true;
+        }
+        // Jika tidak ada halaman yang pernah dibuka
+        // maka akan keluar dari activity (tutup aplikasi)
+        return super.onKeyDown(keyCode, event);
+    }
+}
