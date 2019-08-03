e71c866fe740ffb3ab4369cd1419897685a0f408
zzulfiqor <zzulfiqor@gmail.com>
2019-08-04 00:29:28 +0700

    finishing

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     WebView view;
+    ProgressBar bar;
+
+    //URL Web yang diinginkan
+    String URL = "https://islam-kita.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //inisialisasi component
         view = findViewById(R.id.webView);
+        bar = findViewById(R.id.progressBar);
+
+        //pengaturan webview
         view.getSettings().setJavaScriptEnabled(true);
         view.setWebViewClient(new MyBrowser());
-//        view.loadUrl("https://zzulfiqor.github.io/");
 
-        //masukkan alamat yang diinginkan disini
-        view.loadUrl("http://hadits.santrisoft.com/");
+        //load url
+        view.loadUrl(URL);
 
 
     }
@@ -30,6 +45,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
         }
     }
 
@@ -43,4 +69,8 @@ public class MainActivity extends AppCompatActivity {
         // maka akan keluar dari activity (tutup aplikasi)
         return super.onKeyDown(keyCode, event);
     }
+
+
 }
+
+
---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView splash;
+
+    private int waktu_loading = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        splash = findViewById(R.id.splash_image);
+
+
+        //handler untuk auto redirect ke main activity dalam waktu tertentu
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+
+    }
+}
